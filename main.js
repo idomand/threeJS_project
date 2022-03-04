9df89cf1c8650666d2b72467d14e52386789ca02
Ido Mandelman <idomand@gmail.com>
2022-03-04 15:15:02 +0100

    adding orbit control

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import "./style.css";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
 
@@ -21,13 +22,30 @@ camera.position.setZ(30);
 renderer.render(scene, camera);
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshBasicMaterial({
+const material = new THREE.MeshStandardMaterial({
   color: 0xff6347,
-  wireframe: true,
 });
+
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
+
+const pointLight = new THREE.PointLight(0xffffff);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+scene.add(pointLight);
+scene.add(ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(gridHelper);
+
+scene.add(lightHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
 
 function animate() {
   requestAnimationFrame(animate);
@@ -35,7 +53,7 @@ function animate() {
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
-
+  controls.update();
   renderer.render(scene, camera);
 }
 animate();
